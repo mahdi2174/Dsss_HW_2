d47029e02886d0bda4d8b9213b00c7e54bb67fdd
mahdi2174 <mahdibargh21@gmail.com>
2023-11-13 22:31:00 +0000

    I updated tests_math_quiz.py file

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -14,17 +14,24 @@ class TestMathGame(unittest.TestCase):
 
     def test_function_B(self):
         # TODO
-        pass
+        # Test if function_C correctly computes the arithmetic operation
+        result = function_B(2, 3)
+        self.assertEqual(result, 5)  # Assert that the result should be 5 for inputs 2 and 3
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
                 ''' TODO add more test cases here '''
+                (8, 4, '-', '8 - 4', 4),  # Test subtraction
+                (3, 5, '*', '3 * 5', 15),  # Test multiplication
+                (10, 2, '/', '10 / 2', 5),  # Test division
             ]
-
+            
+            
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+              problem, answer = function_C(num1, num2, operator)
+              self.assertEqual(problem, expected_problem)  # Assert that the generated problem is as expected
+              self.assertEqual(answer, expected_answer)  # Assert that the computed answer is as expected
 
 if __name__ == "__main__":
     unittest.main()
